779f4f417d7cbe348044cd0523fb4ca91147368f
DanielledeAvila <danivandinha1@gmail.com>
2024-11-29 10:50:49 -0300

    Subindo o responsivo

--- src/Pages/Doados/Doados.jsx
+++ src/Pages/Doados/Doados.jsx
@@ -1,11 +1,22 @@
 import s from "./doados.module.scss"
-
+import livro from '../../assets/livro.png'
 
 export default function Doados(){
     return(
-        <section>
-            <h2>Doados</h2>
+        <section className={s.boxDoados} >
+            <h2>Livros Doados</h2>
+            <section className={s.boxcard} >
+                <article>
+                    <img src={livro} alt="Imagem do livro O protagonista" />
+                    <h3>O protagonista</h3>
+                    <p>Susanne Andrade</p>
+                    <p>Ficção</p>
+                </article>
+            </section>
         </section>
     )
 }
+
+
+
 
