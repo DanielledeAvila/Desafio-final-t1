55d4239a860d2541e3b53de080df394d38d1297f
DanielledeAvila <danivandinha1@gmail.com>
2025-04-02 10:38:08 -0300

    desafiofinal

--- src/Pages/Doados/Doados.jsx
+++ src/Pages/Doados/Doados.jsx
@@ -1,25 +1,46 @@
 import s from "./doados.module.scss"
 import livro from '../../assets/livro.png'
+import axios from 'axios'
+import {useState,useEffect} from 'react'
+
 
 export default function Doados(){
+    // Guardar os dados com useState no front
+    const [livros, setLivros] = useState([])
+    const getLivros = async () => {
+        const response = await axios.get("https://api-livros-t1.onrender.com/livros")
+        // Variável response recebe os dados da linha acima filtrada por dados e põe no useState
+        setLivros(response.data)
+        console.log
+    }
+
+    // Precisa de aero function porque ele não faz sozinho
+    useEffect(()=>{
+        getLivros()
+    },[])
+    // o colchete observa uma vez só, quando troca de página
+
+
     return(
         <section className={s.boxDoados} >
             <h2>Livros Doados</h2>
             <section className={s.boxcard} >
-                <article>
+                {/* <article>
                     <img src={livro} alt="Imagem do livro O protagonista" />
                     <h3>O protagonista</h3>
                     <p>Susanne Andrade</p>
                     <p>Ficção</p>
-                </article>
-
-                <article>
-                    <img src={livro} alt="Imagem do livro O protagonista" />
-                    <h3>O protagonista</h3>
-                    <p>Susanne Andrade</p>
-                    <p>Ficção</p>
-                </article>
-                
+                </article> */}
+                {/* Mapeia os dados e ordena*/}
+                {livros.map((item)=>(
+                    <article>
+                        <img src={item.imagem_url} alt= {`Imagem do livro: ${item.titulo}`}/>
+                        <h3>{item.titulo}</h3>
+                        <p>{item.categoria}</p>
+                        <p>{item.autor}</p>
+                    </article>
+                ))}
+                        
             </section>
         </section>
     )
--- src/Pages/QueroDoar/QueroDoar.jsx
+++ src/Pages/QueroDoar/QueroDoar.jsx
@@ -1,24 +1,68 @@
 import s from './querodoar.module.scss'
 import livro from '../../assets/Vector.png'
+import axios from 'axios'
+import {useState} from 'react'
 
-
+// Guardar os dados
 export default function QueroDoar(){
+    const [titulo, setTitulo] = useState("")
+    const [categoria, setCategoria] = useState("")
+    const [autor, setAutor] = useState("")
+    const [imagem_url, setImagem_url] = useState("")
+
+    // função async fica esperando para depois enviar
+    const enviarDados = async()=>{
+        // link da minha API abaixo, com barra doar:
+        const urlApi = "https://api-livros-t1.onrender.com/doar"
+        // abaixo um objeto, como os dados são enviados, tratados, formatados.
+        const dadosEnviar = {
+            titulo,
+            categoria,
+            autor,
+            imagem_url
+        }
+        // axios é uma biblioteca que ajuda a consumir a Api
+        await axios.post(urlApi,dadosEnviar)        
+        // mensagem de alerta de envio
+        alert('Livro enviado!')
+        // Aqui limpa os campos após enviar
+        setTitulo("")
+        setCategoria("")
+        setAutor("")
+        setImagem_url("")        
+    }
+
+    // funções para pegar os dados e importar pro useState
+    const capturaTitulo = (e) => {
+        setTitulo(e.target.value)
+    }
+    const capturaCategoria = (e) => {
+        setCategoria(e.target.value)
+    }
+    const capturaAutor = (e) => {
+        setAutor(e.target.value)
+    }
+    const capturaImagem = (e) => {
+        setImagem_url(e.target.value)
+    }
+
     return(
         <section className={s.principal}>
             <section className={s.container} >
                 <h2>Por favor, preencha o formulário com suas informações e as informações do Livro</h2>
-                <form action="">
+                <form onSubmit={(e)=> e.preventDefault()}>
                     <div>
                         <img src={livro} alt="icone de um livro aberto"/>
                         <h3>Informações do Livro
-                        </h3>  
+                        </h3>                         
                     </div>
-                    <input type="text" placeholder='Título'/>
-                    <input type="text" placeholder='Categoria'/>
-                    <input type="text" placeholder='Autor'/>
-                    <input type="text" placeholder='Link da imagem'/>
-                    <input type="submit" value="Doar"/>
-                </form>
+                    {/* onchange captura o que é digitado */}
+                    <input type="text" placeholder='Título' onChange={capturaTitulo} value={titulo}/>
+                    <input type="text" placeholder='Categoria' onChange={capturaCategoria} value={categoria}/>
+                    <input type="text" placeholder='Autor' onChange={capturaAutor} value={autor}/>
+                    <input type="text" placeholder='Link da imagem' onChange={capturaImagem} value={imagem_url}/>
+                    <input type="submit" value="Doar" onClick={enviarDados}/>                
+                </form>                
             </section>
         </section>
     )
